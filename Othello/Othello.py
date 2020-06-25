ff7d21978649d886aa41bdebee7bb7c0a923588c
beyanon <tokabe333@gmail.com>
2020-06-25 17:20:29 +0900

    update : マスターとオセロでわけたい

--- Othello/Othello.py
+++ Othello/Othello.py
@@ -1,16 +1,17 @@
 import sys, os, json
+import OthelloBoard as ob
 sys.path.append(os.getcwd())
 from Util import GameRule as ugr
 from Util import Json as uj
 
 class Othello(ugr.GameRule):
-	def __init__(self, w, h, name1, name2):
+	def __init__(self, n, name1, name2):
 		super().__init__()
-		self.board = [[0 for i in range(w)] for j in range(h)]
+		self.board = ob.OthelloBoard()
 		self.player_names = [name1, name2]
 		self.player_scores = [0] * 2
 	
-	
+
 
 
 def main_othello():
--- test.py
+++ test.py
@@ -1,2 +1,5 @@
-a = [[i * j for i in range(10)] for j in range(3)]
+import numpy as np
+n = 8
+a = np.array([[0] * n for i in range(n)])
+a[3][2] = 334
 print(a)
